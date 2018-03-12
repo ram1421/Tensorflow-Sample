713cf05b61bee9095d6021e2541108bda8ec36b4
bhargava <ram.ved421@gmail.com>
2018-03-12 00:58:43 -0400

    updated Comments

--- custom_estimator.py
+++ custom_estimator.py
@@ -1,51 +1,76 @@
-import argparse
-import tensorflow as tf
+# This example is considering the Dealertrack user data and making a pridection if the deal will be approved or rejected.
+#Note: I have NOT used any PII data from any environment. The datasets (training & testing) were created based on application_dtcan table.
+# What is Tensorflow ? It is a symbolic math library, and also used for machine learning applications such as neural networks.
+#Tensorflow provides API in Python, C++ , Haskell , java and thrid party packages are also available in C#.
+#This below sample is written in Python using python libraries like pandas
 
+# import tensorflow from python
+import tensorflow as tf
 import user_data
 
 #this is the size
-#batch size ????
+#batch size - The number of examples in a one iteration
+#IMP: Smaller batch sizes usually enables to train a model faster at the EXPENSE of accuracy
+#100 is deafult
 batch_size=100
-#train_steps ???
+#train_steps : this argument tells to stop training after the specified number of iterations.
+#IMP: Increasing steps INCREASES the amount of time the model will train.
+#training a model longer does not guarantee a better model
+#1000 is default number of steps
 train_steps=1000
 
-def main(argv):
+#Choosing the right number of steps & batch_size usually requires both experience and experimentation.
 
-   #args = parser.parse_args(argv[1:])
+def main(args):
 
-   # Fetch the data
+   # Step 1 : Fetch the data
    (train_features, train_label), (test_features, test_label) = user_data.load_data()
 
+    # Step 2: Create feature columns to describe data
    # Feature columns describe how to use the input.
    #A feature column is a data structure that tells your model how to interpret the data in each feature
    #set of all possible status in which a deal might be (approved / denied)
    my_feature_columns = []
-   for key in train_features.keys():
-       my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+   for key in train_features.keys(): # Keys -> 'age', 'married', 'salary', 'employment'
+       my_feature_columns.append(tf.feature_column.numeric_column(key=key))# 'age', 'married', 'salary', 'employment'
 
-   # Build 2 hidden layer DNN with 10, 10 units respectively.
+    # STEP 3: Select the type of model
+    # Build 2 hidden layer Deep Neural Network with 4 nodes respectively.- as seen in the image.
+    # It crucial get the correct number of hidden layers , type of classifier( recurrent , convolution NN) and the number of nodes in each hidden layer.
+    # Increasing the number of hidden layers and neurons typically creates a more powerful model, which requires more data to train effectively.
    classifier = tf.estimator.DNNClassifier(
        feature_columns=my_feature_columns,
-       # one hidden layers of 10 nodes each.
-       hidden_units=[10],
+       # Two hidden layers of 4 nodes each.
+       hidden_units=[4,4],
        # The n_classes parameter specifies the number of possible values that the neural network can predict.
        # Since we have 2 status , we set n_classes to 2.
        n_classes=2)
 
-   # Train the Model.
+    # STEP : 4 we start the training of Model using trainig data ( features + labels)with batch_size of 100 and train_steps of 1000
+    # Based on the type of the activation functions ( step or sigmoid function) weights are adjusted using back propogation.
+   # This model uses forward propogation
+
+   # number of train_steps / batch_size are number of iterations ( 1000/100 = 10 iteratons) and increasing step by 100 in each iteration.
+   #INFO:tensorflow:Loss for final step: 0.0049694357.
+   # Train the Model. this type of training is an example of supervised machine learning as we have dataset with labels.
    classifier.train(
        input_fn=lambda:user_data.train_input_fn(train_features, train_label,
                                                 batch_size),
        steps=train_steps)
 
-   # Evaluate the model.
-   eval_result = classifier.evaluate(
-       input_fn=lambda:user_data.eval_input_fn(test_features, test_label,
-                                               batch_size))
 
+   # STEP 5 : Evaluate the model is used to determine how effectively the model makes predictions.
+   # We now provide the test data ( features and label)
+   eval_result = classifier.evaluate(
+       input_fn=lambda:user_data.eval_input_fn(test_features,
+                                               test_label,
+                                               batch_size))
+    #Test set accuracy: 1.000
    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
-   # Generate predictions from the model
+
+
+   # STEP :6  Generate predictions from the model
    expected = ['denied']
    predict_x = {
        'age': [29],
@@ -54,12 +79,12 @@ def main(argv):
        'employment': [2],
    }
 
+    #IMP: We are not passing in the label value, as we don't know the value
    predictions = classifier.predict(
        input_fn=lambda:user_data.eval_input_fn(predict_x,
                                                labels=None,
                                                batch_size=batch_size))
 
-   print(predictions)
    for pred_dict, expec in zip(predictions, expected):
        template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
 
@@ -71,5 +96,6 @@ def main(argv):
 
 
 if __name__ == '__main__':
+    # logging info too
    tf.logging.set_verbosity(tf.logging.INFO)
    tf.app.run(main)
--- user_data.py
+++ user_data.py
@@ -1,39 +1,52 @@
+
+#pandas is being used for data manipulation and analysis
 import pandas as pd
 import tensorflow as tf
 
-#TRAIN_URL = "http://download.tensorflow.org/data/iris_training.csv"
-#TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
-#TRAIN_URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vR-M1JDEWwh9XBIf_ImdvzdWQTpjAhGzKK8cCPhhdnsN6nB8TEkOYitBjchc9CrjpB_AFmp0G8OpZof/pub?gid=853462602&single=true&output=csv"
-#TEST_URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vR-M1JDEWwh9XBIf_ImdvzdWQTpjAhGzKK8cCPhhdnsN6nB8TEkOYitBjchc9CrjpB_AFmp0G8OpZof/pub?gid=853462602&single=true&output=csv"
+#The training set and test set started out as a single data set
+#IMP:Adding examples to the training set usually builds a better model,adding more examples to the test enables us to better gauge the model's effectiveness
+#The training set contains the examples that we'll use to train the model;
+TRAIN_PATH= "E:\Learn\TensorFlow\TensorFlow_GettingStared\TensorFlow_Dataset_Train.csv"
+# The test set contains the examples that we well  use to evaluate the trained model's effectiveness.
+TEST_PATH= "E:\Learn\TensorFlow\TensorFlow_GettingStared\TensorFlow_Dataset_Test.csv"
 
-TRAIN_URL="E:\Learn\TensorFlow\TensorFlow_GettingStared\TensorFlow_Dataset_Test.csv"
-TEST_URL="E:\Learn\TensorFlow\TensorFlow_GettingStared\TensorFlow_Dataset_Test.csv"
-
+#age,married,salary,employment - > Variables
+#status -> Label
 CSV_COLUMN_NAMES = ['age', 'married',
                    'salary', 'employment', 'status']
+#type of classes ( on this case we have 2 types of status ( denied or approved))
 status = ['denied', 'approved']
 
-def maybe_download():
+def download_data():
    #https://www.tensorflow.org/api_docs/python/tf/keras/utils/get_file
-   #If an absolute path /path/to/file.txt is specified the file will be saved at that location.
-   train_path = tf.keras.utils.get_file(TRAIN_URL.split('/')[-1], TRAIN_URL)
-   test_path = tf.keras.utils.get_file(TEST_URL.split('/')[-1], TEST_URL)
+   #Keras utility in tensorflow is used to downloads a file from a URL if it not already in the cache.
+   #As this path is from a file system, file will not be downloaded into keras cache.
+   #train_path - >E:\Learn\TensorFlow\TensorFlow_GettingStared\TensorFlow_Dataset_Train.csv
+   train_path = tf.keras.utils.get_file(TRAIN_PATH.split('/')[-1], TRAIN_PATH)
+
+   #test_path ->E:\Learn\TensorFlow\TensorFlow_GettingStared\TensorFlow_Dataset_Test.csv
+   test_path = tf.keras.utils.get_file(TEST_PATH.split('/')[-1], TEST_PATH)
 
    # print the train , test path
-   ## train_path now holds the pathname: ~/.keras/datasets/iris_training.csv
    return train_path, test_path
 
-def load_data(y_name='status'):
-   """Returns the iris dataset as (train_features, train_label), (test_features, test_label)."""
-   train_path, test_path = maybe_download()
+def load_data(label_name='status'):
 
+   train_path, test_path = download_data()
 
+   # using pandas library, read csv file and replace column values at header at position 0 in csv with CSV_COLUMN_NAMES
    train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
-   # what does these values have....
-   train_features, train_label = train, train.pop(y_name)
+   #seperate features and labels data frames are created
+   train_features, train_label = train, train.pop(label_name)
+   #sample output  - train_features withoutlabel label data
+   #S.NO    age      married   salary   employment
+   #0       30          1        20000   2
+   #sample output  - train_label without header and feature data
+   #0   0
 
+   # using pandas library, read csv file and replace column values at header at position 0 in csv with CSV_COLUMN_NAMES
    test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
-   test_features, test_label = test, test.pop(y_name)
+   test_features, test_label = test, test.pop(label_name)
 
    return (train_features, train_label), (test_features, test_label)
 
